e9ae9a64c2dffe92e2deedae81d2492b13674044
Juri-12 <ju.am.k4@hotmail.com>
2025-02-16 21:39:23 +0000

    Login-SignUp-Pages
    made the sign up and the login pages

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("", views.landing_page, name="landing") #Default page
+    
+    path("", views.landing_page, name="landing"), #Default page
+    
+    path("register", views.register, name="register"),
+   
+    path('my-login', views.my_login, name="my-login"),
+    
+    path('dashboard', views.dashboard, name="dashboard"),
+   
+    path('user-logout', views.user_logout, name='user-logout'),
 ]
+   
+ 
--- core/views.py
+++ core/views.py
@@ -1,4 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import CreateUserForm, LoginForm
+
+
+#- authenticate models and functions
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
+
+
+
+
+# Create your views here.
 
 def landing_page(request):
     return render(request, 'landing.html')
+
+
+
+def register(request):
+
+  form = CreateUserForm()
+  
+  if request.method == "POST":
+      
+      form = CreateUserForm(request.POST)
+    
+           
+      if form.is_valid():
+               
+               form.save()
+               
+               return redirect('my-login')
+               
+    
+  context = {'registerform': form}
+    
+    
+  return render(request, 'register.html', context=context)
+
+
+def my_login(request):
+    
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        
+        form = LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+            
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+    
+    context = {'loginform':form}
+    
+    
+    return render(request, 'my-login.html', context=context)
+
+
+def dashboard(request):
+    
+    return render(request, 'dashboard.html')
+
+def user_logout(request):
+    
+    auth.logout(request)
+    
+    return redirect("")
+
+ 
