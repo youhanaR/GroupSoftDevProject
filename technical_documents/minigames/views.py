9eebaf4d255b043a8a21bfbcfbf9e2b0a9eefdee
Jem <challisjem@gmail.com>
2025-03-17 00:17:51 +0000

    Primarily Rami's work. Added Recycle Rush minigame. Moved static files back into individual apps.

--- technical_documents/minigames/views.py
+++ technical_documents/minigames/views.py
@@ -1,4 +1,4 @@
-# Author: Jem Challis
+# Author: Jem Challis, Rami Youhana
 
 from django.shortcuts import render
 from .forms import NewGameScore
@@ -31,3 +31,31 @@ def str_end(request):
             form.save()
 
     return render(request, 'str_end.html', {'score': score, 'target': target})
+
+def recycle_rush_intro(request):
+    return render(request, 'recycle_rush_intro.html')
+
+def recycle_rush_game(request):
+    return render(request, 'recycle_rush_game.html')
+
+def recycle_rush_end(request):
+    score = request.POST.get('score', 0)
+    target = request.POST.get('target', 10)
+    user = request.user
+    game = Game.objects.get(name = "Recycle Rush")
+
+    existing_score = GameScore.objects.filter(user=user, game=game).first()
+    if existing_score:
+        # If the score exists, check if the new score is higher
+        if int(score) > int(existing_score.score):
+            # If the new score is higher, update the existing entry
+            existing_score.score = score
+            existing_score.save()
+    else:
+        game_score = GameScore(user=user, game=game, score=score)
+        game_score.save()
+        form = NewGameScore()
+        if form.is_valid():
+            form.save()
+
+    return render(request, 'recycle_rush_end.html', {'score': score, 'target': target})
--- technical_documents/napoleons_adventure/settings.py
+++ technical_documents/napoleons_adventure/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'core' /'static']
+STATICFILES_DIRS = [BASE_DIR / 'core' /'static', BASE_DIR / 'mingames' /'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
