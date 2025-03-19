eaf58afa69f52527d5d1d6ae2eb844ab84eb110e
Juri-12 <ju.am.k4@hotmail.com>
2025-03-19 06:04:00 +0000

    user-profile
    -add user profile pics

--- technical_documents/core/models.py
+++ technical_documents/core/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from minigames.models import Game
-
+import random
 # Create a location table in the database ~ Ameera
 class Location(models.Model):
     name = models.CharField(max_length=100)
@@ -24,9 +24,25 @@ class UserProgress(models.Model):
         return f"{self.user.username} - {self.location.name} - {'Completed' if self.completed else 'Incomplete'}"
     
 
+# Selects a random a icon for profile image from the profile_pics 
+def random_cat_image():
+    cat_images = [
+        "images/profile_pics/cat1.png",
+        "images/profile_pics/cat2.png",
+        "images/profile_pics/cat3.png",
+        "images/profile_pics/cat4.png",
+        "images/profile_pics/cat5.png",
+        "images/profile_pics/cat6.png",
+        "images/profile_pics/cat7.png",
+        "images/profile_pics/cat8.png",
+        "images/profile_pics/cat9.png",
+    ]
+    return random.choice(cat_images)
+
 # Create a profile table in the database ~ Juri
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+    profile_image = models.ImageField(upload_to="profile_pics/", default=random_cat_image)#Each user has a profile with a profile picture that defaults to a random cat image.
+
     def __str__(self):
         return self.user.username
