86b5957fc0ad208dfa7f461d3d0ff7f2eee79a90
Juri-12 <ju.am.k4@hotmail.com>
2025-03-23 03:05:59 +0000

    .

--- technical_documents/minigames/models.py
+++ technical_documents/minigames/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Jem
 class Game(models.Model):
     name = models.CharField(max_length=20)
-    url = models.URLField()  # URL of the minigame page
+    url = models.CharField(max_length=100)  # URL of the minigame page
     def __str__(self):
         return f"{self.name}"
 
